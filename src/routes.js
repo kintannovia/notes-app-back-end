3338c5ee68231dc82d9368c6554a833ca6236d8f
Kintan Novia Azzahra <kintannovia29@gmail.com>
2024-10-02 21:43:37 +0700

    fisrt commit

---
+++ src/routes.js
@@ -0,0 +1,38 @@
+const {
+  addBookHandler, getAllBookHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByHandler,
+  },
+];
+
+module.exports = routes;
